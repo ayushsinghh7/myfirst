5ac2e203fd7b1164e5c64199459ca7b3937f5cf3
ayushsinghh7 <120711977+ayushsinghh7@users.noreply.github.com>
2023-04-13 03:17:54 +0530

    Update database.js

--- exp16/util/database.js
+++ exp16/util/database.js
@@ -1,17 +1,4 @@
-// const mysql=require('mysql2');
-// const pool=mysql.createPool({
-//     host:'localhost',
-//     user:'root',
-//     database:'node-complete',
-//     password:'nicobabu98265'
 
-// });
-
-
-
-
-
-// module.exports=pool.promise();
 
 const  Sequilize =require('sequelize');
 const sequelize=new Sequilize('node-complete','root','',{dialect:'mysql',host:'localhost'});
