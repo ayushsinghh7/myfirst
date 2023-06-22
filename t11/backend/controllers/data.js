9dba72c440f910098be5cc6213a79f67bfcc8538
ayushsinghh7 <rajputa627@gmail.com>
2023-06-22 16:26:15 +0530

    task11

---
+++ t11/backend/controllers/data.js
@@ -0,0 +1,92 @@
+
+
+
+const Data=require('../models/data');
+var bcrypt=require('bcrypt');
+var j=require('jsonwebtoken');
+function isstringinvalid(string){
+  if(string == undefined ||string.length === 0){
+      return true
+  } else {
+      return false
+  }
+}
+const datapost= async (req,res)=>{
+    try{ 
+     const Email=req.body.pro;
+  
+     const  Name=req.body.price;
+
+     const Password=req.body.cat;
+ 
+      console.log(Name);
+      const saltrounds=10;
+     bcrypt.hash(Password,saltrounds,async(err,hash)=>{
+
+     //const data=await Data.create({Name:Price,Email:Name,Password:Cat+hash});
+     await Data.create({Name,Email,Password:hash});
+
+     //res.status(201).json({newUserDetail:data});
+     res.status(201).json({message:"sucessfully created new user"});
+    })
+     
+     
+ }
+  catch(err){
+    console.log('error')
+ res.status(500).json({
+   error:err
+ })
+   }
+ }
+//  exports.getdata=async (req,res)=>{
+//   try{ 
+//       const user= await Data.findAll();
+//       res.status(201).json({all:user})
+//   }
+//   catch(err){
+//       res.status(500).json({
+//         error:err
+//       })
+//   }
+// };
+     const generatetoken=(id,Name,ispremiumuser)=>{
+  return j.sign({batumId:id,Name:Name,ispremiumuser},'secretkey')
+}
+const login = async (req, res) => {
+  try{
+  const Email=req.body.Email;
+  const Password=req.body.Password;
+
+  console.log(req.body.Email)
+  if(isstringinvalid(Email) || isstringinvalid(Password)){
+     return res.status(400).json({message: 'EMail idor password is missing ', success: false})
+  }
+  console.log(Password);
+  const user  = await Data.findAll({ where : { Email }})
+      if(user.length > 0){
+         bcrypt.compare(Password, user[0].Password, (err, result) => {
+         if(err){
+          throw new Error('Something went wrong')
+       
+         }
+          if(result === true){
+              return res.status(200).json({success: true, message: "User logged in successfully",token:generatetoken(user[0].id,user[0].Name,user[0].ispremiumuser)})
+          }
+          else{
+          return res.status(200).json({success: false, message: 'Password is incorrect'})
+         }
+      })
+      } else {
+          return res.status(404).json({success: false, message: 'User Doesnot exitst'})
+      }
+  }catch(err){
+      res.status(500).json({message: err, success: false})
+  }
+}
+module.exports={
+  datapost,
+  login,
+  generatetoken
+  
+}
---
+++ t11/backend/controllers/expense.js
@@ -0,0 +1,59 @@
+//const Expense = require('../models/expense');
+const Expenses = require('../models/expense');
+
+exports.addexpense = async (req, res) => {
+    console.log(req.body.Expense)
+    
+    
+    try{
+    const { Expense, Desc, cat } = req.body;
+//console.log(batumId);
+    if(Expense == undefined || Expense.length === 0 ){
+       
+        return res.status(400).json({success: false, message: 'Parameters missing'})
+    }
+    
+    const data=await Expenses.create({ Expense, Desc, cat,batumId:req.user.id}) ;
+     res.status(201).json({newUserDetail:data});
+        //return res.status(201).json({Expense, success: true } );
+    
+    }
+    catch(err) {
+         console.log(err)
+        return res.status(500).json({success : false, error: err})
+       
+    }
+}
+
+exports.getexpense = async (req, res)=> {
+    
+   
+    try{ 
+       var all=await Expenses.findAll({ where : { batumId: req.user.id}})
+            return res.status(200).json({all, success: true})
+          }
+          catch(err){
+              res.status(500).json({
+                error:err
+              })
+          }
+        };
+        exports.deleteexpense = async  (req, res) => {
+            try{
+            const expenseid = req.params.expenseid;
+             console.log(expenseid);
+            if(expenseid == undefined || expenseid.length === 0){
+                return res.status(400).json({success: false, })
+            }
+           
+           var t= await Expenses.destroy({where: { id: expenseid, batumId: req.user.id }})
+                if(t === 0){
+                    return res.status(404).json({success: false, message: 'Expense doenst belong to the user'})
+                }
+                return res.status(200).json({ success: true, message: "Deleted Successfuly"})
+            }catch(err){
+                console.log(err);
+                return res.status(500).json({ success: true, message: "Failed"})
+            }
+        }
+    
